7c94a8f6176077ba026fcd24e2461bc5ff15bf37
Jeremy Foster <jeremy.foster@live.com>
2014-10-15 10:46:31 -0700

    overhauled client code to use hottowel template

---
+++ Code/FrayedKnot.Client/app/dashboard/dashboard.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'dashboard';
+    angular.module('app').controller(controllerId, ['common', 'datacontext', dashboard]);
+
+    function dashboard(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.news = {
+            title: 'Knotty News',
+            description: 'Knots are fun and useful!'
+        };
+        vm.messageCount = 0;
+        vm.people = [];
+        vm.title = 'Dashboard';
+
+        activate();
+
+        function activate() {
+            var promises = [getMessageCount(), getKnots(), getCategories()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Dashboard View'); });
+        }
+
+        function getMessageCount() {
+            return datacontext.getMessageCount().then(function (data) {
+                return vm.messageCount = data;
+            });
+        }
+
+        function getKnots() {
+            return datacontext.getKnots().then(function (data) { return vm.knots = data; });
+        }
+
+        function getCategories() {
+            return datacontext.getCategories().then(function (data) { return vm.categories = data; });
+        }
+    }
+})();
