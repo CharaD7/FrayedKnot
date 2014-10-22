17a9dbbd239190194e883f6e12298af8a0d6f98f
GeekTrainer <chrhar@microsoft.com>
2014-10-22 12:16:06 -0700

    updates

---
+++ Christopher Demos/BundlingDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BundlingDemo.Startup))]
+namespace BundlingDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
